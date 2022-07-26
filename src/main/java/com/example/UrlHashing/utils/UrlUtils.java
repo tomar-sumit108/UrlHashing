0464db95d17fec7375114c04253440ecb3a3d4c4
Sumit Tomar <32814832+tomar-sumit108@users.noreply.github.com>
2022-07-26 07:26:36 +0530

    Utils bug Fix

--- src/main/java/com/example/UrlHashing/utils/UrlUtils.java
+++ src/main/java/com/example/UrlHashing/utils/UrlUtils.java
@@ -1,15 +1,12 @@
 package com.example.UrlHashing.utils;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
 public class UrlUtils {
 	public static final String BASE_STRING = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
 	public static int decode(String url) {
 		int id = 0;
 		for(int i=0; i<url.length(); i++) {
 			int ch = url.charAt(i);
-			id = id*(BASE_STRING.length()) + BASE_STRING.indexOf(ch)
+			id = id*(BASE_STRING.length()) + BASE_STRING.indexOf(ch);
 		}
 		return id;
 	}
